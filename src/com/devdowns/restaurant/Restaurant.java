c732bf6d7eb3ec0a0c396badf623a973f9a004eb
Devon Downs <devondowns93@gmail.com>
2021-07-26 11:02:00 -0600

    added todo in main

--- src/com/devdowns/restaurant/Restaurant.java
+++ src/com/devdowns/restaurant/Restaurant.java
@@ -3,6 +3,9 @@ package com.devdowns.restaurant;
 public class Restaurant {
 
     public static void main(String[] args) {
-        
+        /*TODO 1 kitchen thread to produce dishes from an enum
+          Multiple customers can eat dishes as they're made
+          You can have customers clients that only eat certain dishes
+         */
     }
 }
